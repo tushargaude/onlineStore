8cf21611865d484f290ec5f0f7e587db169ae1d2
tushargaude <tushargaude@gmail.com>
2022-10-21 15:52:38 +0530

    Cart Model Creation

---
+++ migrations/20221021102133-create-cart.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      itemId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        reference : {
+          model : 'items',
+          key : "id"
+        }
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('carts');
+  }
+};
