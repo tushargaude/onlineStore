7bf27c17cacb48bb0c361074115580b28f0f9bac
tushargaude <tushargaude@gmail.com>
2022-10-21 15:54:07 +0530

    Order Model Creation

---
+++ migrations/20221021102307-create-order.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      cartId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        reference : {
+          model : 'cart',
+          key : "id"
+        }
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('orders');
+  }
+};
